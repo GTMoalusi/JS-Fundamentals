70b0843361eb0267e9268d6cf3a18131338a249d
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-07-11 20:19:51 +0200

    initial commit: value of my argument

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,9 @@
-const numberOfArgumentsPassed = process.argv.length - 2;;
+let numberOfArgumentsPassed = process.argv.length - 2;;
 
 if (numberOfArgumentsPassed === 0) {
 	console.log(`${numberOfArgumentsPassed} No argument`);
 }else if (numberOfArgumentsPassed === 1) {
+	let firstArgument = process.argv[2];
 	console.log(`${numberOfArgumentsPassed} Argument found`);
 }else{
 	console.log(`${numberOfArgumentsPassed} Arguments found`);
