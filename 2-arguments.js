c14bd597db9a3cec9aea6cb4ff13a6f5e4994740
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-07-11 19:11:02 +0200

    Initial commit: add 2-arguments

---
+++ 2-arguments.js
@@ -0,0 +1,7 @@
+if (numberOfArgumentsPassed == 0) {
+	console.log("No argument");
+}else if (numberOfArgumentsPassed == 1) {
+	console.log("Argument found");
+}else{
+	console.log("Arguments found");
+}
