da5ae7745aa7f6319a7b23a2aa3b6511ea668ba8
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-07-12 11:54:16 +0200

    initial commit: Arrays- multi line.

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,10 @@
 let lines = ["C is fun", "Python is amazing", "JavaScript is amazing"];
 
-let output = lines.join("\n");
+let output = "";
+
+for (let i = 0; i < lines.length; i++) {
+  output += lines[i];
+  output += "\n";
+}
 
 console.log(output);
