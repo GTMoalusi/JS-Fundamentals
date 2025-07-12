53032431f655c23f9080b3f65b560b7726f9e63f
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-07-12 13:04:43 +0200

    initial commit: Write a script that prints x times “C is fun”

---
+++ 7-multi_c.js
@@ -0,0 +1,17 @@
+let myStringInput = process.argv[2];
+
+let isANumber = parseInt(myStringInput);
+
+if (isNaN(isANumber)){
+	console.log("Missing number of occurrences");
+}else{
+	let outputString = "";
+
+	for (let i = 0; i < isANumber; i++) {
+   		outputString += "C is fun\n";
+	}
+
+  console.log(outputString);
+}
+
+
