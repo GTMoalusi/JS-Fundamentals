4199fb6f420ff7a0c3192d8f0e6e1b16c97128fc
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-07-12 13:40:05 +0200

    initial commit: Write a script that prints x times “C is fun

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -5,10 +5,13 @@ let isANumber = parseInt(myStringInput);
 if (isNaN(isANumber)){
 	console.log("Missing number of occurrences");
 }else{
-	let outputString = "";
+	let linesToPrint = [];
+
 	for (let i = 0; i < isANumber; i++){
-   		outputString += "C is fun\n";
+   		linesToPrint.push("C is fun");
 	}
+
+	let outputString = linesToPrint.join('\n');
   	console.log(outputString);
 }
 
